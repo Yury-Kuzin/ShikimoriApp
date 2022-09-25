0b88e41b317b33020bf6dc517d82d7689fd4ae28
a.boev.it@gmail.com <#gi32@+1>
2022-09-25 12:54:05 +0300

    Add project files.

---
+++ ShikimoriApp/Models/Anime.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace ShikimoriApp.Models
+{
+    class Anime
+    {
+        public class ImageList
+        {
+            [JsonPropertyName("original")]
+            public string Original { get; set; }
+            [JsonPropertyName("preview")]
+            public string Preview { get; set; }
+            [JsonPropertyName("x96")]
+            public string X96 { get; set; }
+            [JsonPropertyName("x48")]
+            public string X48 { get; set; }
+        }
+        [JsonPropertyName("id")]
+        public int Id { get; set; } = 0;
+        [JsonPropertyName("name")]
+        public string Name { get; set; }
+        [JsonPropertyName("russian")]
+        public string Russian { get; set; }
+        [JsonPropertyName("url")]
+        public string URL { get; set; }
+        [JsonPropertyName("kind")]
+        public string Kind { get; set; }
+        [JsonPropertyName("score")]
+        public string Score { get; set; }
+        [JsonPropertyName("status")]
+        public string Status { get; set; }
+        [JsonPropertyName("episodes")]
+        public int Episodes { get; set; }
+        [JsonPropertyName("episodes_aired")]
+        public int EpisodesAired { get; set; }
+        [JsonPropertyName("aired_on")]
+        public string AiredOn { get; set; }
+        [JsonPropertyName("released_on")]
+        public string ReleasedOn { get; set; }
+        [JsonPropertyName("image")]
+        public ImageList? Image { get; set; }
+    }
+}
---
+++ ShikimoriApp/ShikimoriController.cs
@@ -0,0 +1,43 @@
+﻿using ShikimoriApp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace ShikimoriApp
+{
+    /// 
+    /// api/animes
+    /// 
+    class ShikimoriController
+    {
+        private const string URL = "https://shikimori.one/";
+        private HttpClient httpClient;
+
+        public ShikimoriController()
+        {
+            httpClient = new HttpClient();
+            httpClient.BaseAddress = new Uri(URL);
+        }
+
+        public List<Anime>? GetAnimes()
+        {
+            // season
+            // page
+            string param = "api/animes?limit=50&page=250";
+            List<Anime> animes = new List<Anime>();
+            HttpResponseMessage response = httpClient.GetAsync(param).Result;
+            if (response.IsSuccessStatusCode)
+            {
+                string json = response.Content.ReadAsStringAsync().Result;
+                animes = JsonSerializer.Deserialize<List<Anime>>(json); 
+            }
+
+            return animes;
+        }
+    }
+}
