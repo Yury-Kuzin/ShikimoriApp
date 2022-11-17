1614ef5afbeabf26e5141094d7369c19753c3dbd
KuzinProg <prototyp2002@gmail.com>
2022-11-17 15:49:04 +0700

    Работа с классом AnimeInfo

--- ShikimoriApp/Models/AnimeInfo.cs
+++ ShikimoriApp/Models/AnimeInfo.cs
@@ -11,6 +11,16 @@ namespace ShikimoriApp.Models
     {
         [JsonPropertyName("description")]
         public string? Description { get; set; }
+        [JsonPropertyName("url")]
+        public string? Url { get; set; }
+        [JsonPropertyName("rating")]
+        public string? Rating { get; set; }
+        [JsonPropertyName("japanese")]
+        public string? Japanese { get; set; }
+        [JsonPropertyName("screenshots")]
+        public string? Screenshots { get; set; }
+
+
 
     }
 }
